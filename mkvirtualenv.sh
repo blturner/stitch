990e146eb5dbb20c0a918e841d058bd46840c35e
Benjamin Turner <benturn@gmail.com>
2011-12-06 23:44:02 -0800

    added shell script for building virtualenvs and cleaned up templates for custom pythonpath

---
+++ mkvirtualenv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Usage:
+# $ mkvirtualenv.sh [--rebuild] virtualenv_name project_dir
+
+# TODO: Currently assumes WORKON_HOME is set.
+# TODO: Implement [--rebuild] option
+
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+export PIP_RESPECT_VIRTUALENV=true
+
+PROJECT_DIR=$2
+VM_DIR="$WORKON_HOME/$1" # Location of the virtualenv
+
+# Create the virtualenv if it doesn't exist
+if [ -d "$VM_DIR" ]
+  then
+    echo "$VM_DIR already exists."
+  else
+    virtualenv $VM_DIR
+fi
+
+PIP_REQUIREMENTS=`find $PROJECT_DIR -name requirements.txt`
+`source $VM_DIR/bin/activate && pip install -r $PIP_REQUIREMENTS`
--- stage.py
+++ stage.py
@@ -1,43 +1,74 @@
+#!/usr/bin/env python
 import os, pprint, urllib, yaml
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('templates'))
 
 def main():
+    """
+    Setup a staging environment.
+    
+    `python stage.py [--rebuild]`
+    
+    --rebuild: Refresh the virtualenv and reinstall all packages with pip.
+    
+    Depends on:
+    - Git
+    - Pip
+    - Virtualenv
+    """
     f = open("staging.yml")
     conf = yaml.load(f.read())
     f.close()
     
+    # TODO: Check for these paths if they are set in staging.yml
     apache_dir = "/Users/bturner/Projects/staging/httpd"
     wsgi_dir = "/Users/bturner/Projects/staging/wsgi"
     
-    for host, hostconfig in conf['hosts'].iteritems():
-        print host
-        print hostconfig
+    # for host, hostconfig in conf['hosts'].iteritems():
+        # print host
+        # print hostconfig
     
     # Generate Apache confs
     for site, sitedict in conf['sites'].iteritems():
+        pythonpath = sitedict.get('pythonpath', conf['sites_defaults']['pythonpath'])
+        # if `pythonpath` is a string, return it as a list
+        if isinstance(pythonpath, str):
+            pythonpath = [pythonpath]
         settings_dir = "staging_settings/%s" % site
-        
-        pythonpath = sitedict.get('pythonpath')
         settings_overrides = {}
+        
+        # Create the virtualenv
+        project_dir = conf['hosts']['rembrandt'].get('project_dir') + '/' + conf['sites'][site].get('project_name')
+        print "project_dir = %s" % project_dir
+        os.system('./mkvirtualenv.sh %s %s' % (site, project_dir))
+        
+        # TODO: Whoa, this is crazy. Find out the virtualenv `site-packages`
+        # dir so it can be added to the wsgi path
+        cmd = 'source `which virtualenvwrapper.sh` && workon %s && \
+            cdsitepackages && pwd' % site
+        fin, fout = os.popen4(cmd)
+        for result in fout.readlines():
+            sp = result.rstrip()
         
         context = {
             'pythonpath': pythonpath,
+            'sitepackages': sp,
             'site': site
         }
         
         # Generate an apache conf for each host in `staging.yml`
         for host in conf['hosts']:
-            host_apache_dir = 'httpd/%s' % host
-            template = env.get_template('apache/base.conf')
-            
-            if not os.path.exists(host_apache_dir):
-                os.makedirs(host_apache_dir)
-            
-            filename = 'httpd/%s/%s.conf' % (host, site)
-            with open(filename, 'w') as f:
-                f.write(template.render(context))
+            if not host == 'defaults':
+                host_apache_dir = 'httpd/%s' % host
+                template = env.get_template('apache/base.conf')
+                
+                if not os.path.exists(host_apache_dir):
+                    os.makedirs(host_apache_dir)
+                
+                filename = 'httpd/%s/%s.conf' % (host, site)
+                with open(filename, 'w') as f:
+                    f.write(template.render(context))
         
         # Generate wsgi conf
         template = env.get_template('wsgi/base.conf')
@@ -50,7 +81,7 @@ def main():
         
         pp = pprint.PrettyPrinter()
         settings_overrides.update(sitedict)
-        settings_overrides.update(conf['defaults'])
+        settings_overrides.update(conf['sites_defaults'])
         settings_overrides = [ [k, pp.pformat(v)] for k, v in settings_overrides.iteritems()]
         context = {
             'original_settings': sitedict.get('original_settings'),
@@ -71,9 +102,9 @@ def main():
         with open(filename, 'w') as f:
             f.write(template.render(context))
             f.close()
-        
-        # Restart apache
-        os.system("/usr/bin/sudo apachectl restart")
+    
+    # Restart apache
+    os.system("/usr/bin/sudo apachectl restart")
 
 if __name__ == '__main__':
     main()
