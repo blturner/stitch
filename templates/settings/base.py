04f014ba9a86a3c9113f66ef7c0348da50fa9d72
Benjamin Turner <benturn@gmail.com>
2012-02-13 22:02:48 -0800

    added debug code to settings template and commented out some excess wsgi bits

--- templates/settings/base.py
+++ templates/settings/base.py
@@ -3,3 +3,13 @@ from {{ original_settings }} import *
 {% for k, v in settings_overrides %}
 {{ k }} = {{ v }}
 {% endfor %}
+
+if DEBUG_TOOLBAR:
+    TEMPLATE_CONTEXT_PROCESSORS = list(TEMPLATE_CONTEXT_PROCESSORS) + ['django.core.context_processors.debug']
+    
+    MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES) + ['debug_toolbar.middleware.DebugToolbarMiddleware']
+    INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TEMPLATE_CONTEXT': True,
+        'INTERCEPT_REDIRECTS': False,
+    }
